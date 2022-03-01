704f5e7841a05ccf078eea713571b34689497366
Sara Cormican <S_Comican@yahoo.com>
2022-03-01 15:01:48 -0500

    using JWT backend

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors')
-const {createUser} = require('./src/users')
+const {createUser, loginUser, getUsers} = require('./src/users')
 const PORT = process.env.PORT || 3001
 
 const app =express()
@@ -8,6 +8,8 @@ app.use(express.json())
 app.use(cors())
 
 app.post('/users', createUser)
+app.post('/user/login', loginUser)
+app.get('/users',getUsers)
 
 app.listen(PORT,()  => {
     console.log (`Listening on port ${PORT}...`)
